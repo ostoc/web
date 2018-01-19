81b16d6177c0c5e3c4e2beb1a56cbb87861301f5
Junyu Pu <pujunyu@gmail.com>
2018-01-19 23:58:03 +0100

    Clean up JS

--- js/main.js
+++ js/main.js
@@ -1,75 +1,44 @@
-$(document).ready(function(){
-  // function isIEorEDGE(){
-  //   if (navigator.appName == 'Microsoft Internet Explorer'){
-  //     return true; // IE
-  //   }
-  //   else if(navigator.appName == "Netscape"){                       
-  //    return navigator.appVersion.indexOf('Edge') > -1; // EDGE
-  //   }       
-  //   return false;
-  // };
+$(document).ready(function () {
+  var flag = 0;
+  var audio = $("audio")[0];
 
-
-  function isieEdge(){
-    // var rv = 0;
+  function isieEdge() {
     if (navigator.appVersion.indexOf('Trident') > -1) {
       return 1;
-    }else if(navigator.appVersion.indexOf('Edge') > -1){
-      return 2; 
+    } else if (navigator.appVersion.indexOf('Edge') > -1) {
+      return 2;
     }
     return 0;
-  }  
-  
+  }
   if (isieEdge()) {
-        $("#alert").css("display","block");
+    $("#alert").css("display", "block");
   };
-
-	var flag = 0;
-
-    $("#mole").click(function () {
-    		if (flag == 0) {
-        		$("#des").fadeTo("fast", "0");
-        		$("#hb-title").fadeTo("slow", "1");
-            $(".atc-button").fadeTo("slow", "0.6");
-      			$(this).html("+");
-      			flag = 1;
-      			console.log(flag);
-      		} else {
-    			$("#des").fadeTo("slow", "1");
-        		$("#hb-title").fadeTo("fast", "0");
-            $(".atc-button").fadeTo("slow", "1");
-      			$(this).html("-");
-      			flag = 0;
-      			console.log(flag);
-      		}
-
-    });
-
-    $("#spinner-form").click(function(){
-     	var mobi_display = $('#mobi-menu').css("display");
-     	// console.log(mobi_display);
-     	if (mobi_display == "none"){
-     		$("#mobi-menu").css("display","block");
-     		// console.log("good");
-     	} else {
-     		$("#mobi-menu").css("display","none");
-     		// console.log("bad");
-     	}
-    	
-    });
-
-    var audio = $("audio")[0];
-	$("#play-name").click(function(){
-		audio.play();
-	});
-
-
-
-
-  // $(".hover").mouseleave(
-  //   function() {
-  //     $(this).removeClass("hover");
-  //   }
-  // );
-
+  $("#mole").click(function () {
+    if (flag == 0) {
+      $("#des").fadeTo("fast", "0");
+      $("#hb-title").fadeTo("slow", "1");
+      $(".atc-button").fadeTo("slow", "0.6");
+      $(this).html("+");
+      flag = 1;
+      console.log(flag);
+    } else {
+      $("#des").fadeTo("slow", "1");
+      $("#hb-title").fadeTo("fast", "0");
+      $(".atc-button").fadeTo("slow", "1");
+      $(this).html("-");
+      flag = 0;
+      console.log(flag);
+    }
+  });
+  $("#spinner-form").click(function () {
+    var mobi_display = $('#mobi-menu').css("display");
+    if (mobi_display == "none") {
+      $("#mobi-menu").css("display", "block");
+    } else {
+      $("#mobi-menu").css("display", "none");
+    }
+  });
+  $("#play-name").click(function () {
+    audio.play();
+  });
 });
